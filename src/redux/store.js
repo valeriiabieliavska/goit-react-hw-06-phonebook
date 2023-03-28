7215cc9632291c589adddaeeb74fc2ae1a4698f3
valeriiabieliavska <106553939+valeriiabieliavska@users.noreply.github.com>
2023-03-28 23:25:19 +0300

    + Redux Persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,40 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import contactReducer from './contactsSlice';
 
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, contactReducer);
+
 const store = configureStore({
-    reducer: {
-        contacts: contactReducer,
-    },
+  reducer: {
+    contacts: persistedReducer,
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }).concat(),
+  devTools: process.env.NODE_ENV !== 'production',
 });
 
-export default store;
+const storeWithPersisted = () => {
+  const persistor = persistStore(store);
+  localStorage.clear();
+  return { store, persistor };
+};
+
+export {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+
+export default storeWithPersisted;
